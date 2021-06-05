803dc65d9d4d4dee0a4b4b37d4766599216f2fd7
patryksla <patryk.slawski@wp.pl>
2021-06-05 21:42:27 +0200

    initial commit

---
+++ pages/registration_page.py
@@ -0,0 +1,110 @@
+from selenium.webdriver.support.select import Select
+
+from pages.base_page import BasePage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import RegistrationPageLocators
+from selenium.webdriver.common.keys import Keys
+
+
+
+class RegistrationPage(BasePage):
+
+    def _verify_page(self):
+        print("Weryfikacja RegistartionPage")
+        wait = WebDriverWait(self.driver, 60)
+        wait.until(EC.presence_of_element_located(RegistrationPageLocators.NAME_INPUT))
+
+    def fill_name(self, name):
+        element = self.driver.find_element(*RegistrationPageLocators.NAME_INPUT)
+        element.send_keys(name)
+
+    def fill_surname(self, surname):
+        element = self.driver.find_element(*RegistrationPageLocators.SURNAME_INPUT)
+        element.send_keys(surname)
+
+    def choose_nationality(self):
+        element = Select(self.driver.find_element(*RegistrationPageLocators.NATIONALITY_INPUT))
+        element.select_by_visible_text("Polska")
+
+
+    def voivodeship(self):
+        wait = WebDriverWait(self.driver, 60)
+        registration_page_element = wait.until(EC.element_to_be_clickable(RegistrationPageLocators.VOIVODESHIP_INPUT))
+        registration_page_element.click()
+        element = self.driver.find_element(*RegistrationPageLocators.VOIVODESHIP_INPUT)
+        element.click()
+        wait = WebDriverWait(self.driver, 60)
+        registration_page_element = wait.until(EC.element_to_be_clickable(RegistrationPageLocators.VOIVODESHIP_CHOOSE))
+        registration_page_element.click()
+        self.driver.find_element(*RegistrationPageLocators.VOIVODESHIP_CHOOSE).click()
+
+    def fill_street(self, street):
+        element = self.driver.find_element(*RegistrationPageLocators.STREET_INPUT)
+        element.send_keys(street)
+
+    def fill_building_number(self, building_number):
+        element = self.driver.find_element(*RegistrationPageLocators.BUILDING_NUMBER_INPUT)
+        element.send_keys(building_number)
+
+    def fill_postal_code(self, postal_code):
+        element = self.driver.find_element(*RegistrationPageLocators.POSTAL_CODE_INPUT)
+        element.send_keys(postal_code)
+
+    def fill_city(self, city):
+        element = self.driver.find_element(*RegistrationPageLocators.CITY_INPUT)
+        element.send_keys(city)
+
+    def fill_phone_number(self, phone_number):
+        element = self.driver.find_element(*RegistrationPageLocators.PHONE_NUMBER_INPUT)
+        element.send_keys(phone_number)
+
+    def fill_email(self, email):
+        element = self.driver.find_element(*RegistrationPageLocators.EMAIL_INPUT)
+        element.send_keys(email)
+
+    def fill_password(self, password):
+        element = self.driver.find_element(*RegistrationPageLocators.PASSWORD_INPUT)
+        element.send_keys(password)
+
+    def fill_password_confirmation(self, password_confirmation):
+        element = self.driver.find_element(*RegistrationPageLocators.PASSWORD_CONFIRMATION_INPUT)
+        element.send_keys(password_confirmation)
+        html = self.driver.find_element_by_tag_name('html')
+        html.send_keys(Keys.END)
+
+
+    def accept_agreement(self):
+        self.driver.implicitly_wait(60)
+        element = self.driver.find_element(*RegistrationPageLocators.ACCEPT_AGREEMENT1)
+        element.click()
+
+    def accept_rodo(self):
+        self.driver.implicitly_wait(60)
+        element = self.driver.find_element(*RegistrationPageLocators.ACCEPT_RODO)
+        element.click()
+
+    def create_account(self):
+        self.driver.implicitly_wait(60)
+        element = self.driver.find_element(*RegistrationPageLocators.CREATE_ACCOUNT)
+        element.click()
+
+    def verify_visible_errors(self, number_of_errors, error_texts):
+        self.driver.implicitly_wait(60)
+
+        error_texts = list(error_texts)
+        error_messages = self.driver.find_elements(*RegistrationPageLocators.ERROR_MESSAGES_DIV)
+
+        visible_error_messages = []
+
+        for error in error_messages:
+            if error.is_displayed():
+                visible_error_messages.append(error)
+        print(visible_error_messages, "Widoczne błędy")
+        print(number_of_errors, "liczba błędów")
+        assert len(visible_error_messages) == number_of_errors
+        error_text_fact = []
+        for i in range(len(visible_error_messages)):
+            error_text_fact.append(visible_error_messages[i].text)
+
+        assert error_texts == error_text_fact
---
+++ test/registration_page_test.py
@@ -0,0 +1,134 @@
+import unittest
+from base_test import BaseTest
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
+from time import sleep
+
+valid_name = "Piotr"
+valid_surname = "Nowak"
+valid_street = "Szkolna"
+valid_building_number = "8"
+valid_postal_code = "00-100"
+valid_city = "Warszawa"
+valid_phone_number = "783546228"
+invalid_email ="piotr2.com"
+valid_email = "emailtestowy@wp.pl"
+valid_password = "Qwerty123!"
+valid_password_confirmation = "Qwerty123!"
+invalid_password = "qwe"
+invalid_password_confirmation = "qwe"
+
+
+class RegistrationPageTest(BaseTest):
+
+    def setUp(self):
+        print("setUp z RegistrationPageTest")
+        super().setUp()
+
+        hp = HomePage(self.driver)
+        hp.click_close_newsletter_btn()
+        hp.click_zaloguj_btn()
+
+        lp = LoginPage(self.driver)
+        lp.click_register_btn()
+
+    def test_incorrect_email(self):
+        rp = RegistrationPage(self.driver)
+        rp.fill_name(valid_name)
+        rp.fill_surname(valid_surname)
+        rp.choose_nationality()
+        #rp.voivodeship()
+        rp.fill_street(valid_street)
+        rp.fill_building_number(valid_building_number)
+        rp.fill_postal_code(valid_postal_code)
+        rp.fill_city(valid_city)
+        rp.fill_phone_number(valid_phone_number)
+        rp.fill_email(invalid_email)
+        rp.fill_password(valid_password)
+        rp.fill_password_confirmation(valid_password_confirmation)
+        rp.accept_agreement()
+        rp.accept_rodo()
+        rp.create_account()
+        rp.verify_visible_errors(1, ["Podany adres email jest nieprawidłowy"])
+
+    def test_empty_name(self):
+        rp = RegistrationPage(self.driver)
+        rp.fill_surname(valid_surname)
+        rp.choose_nationality()
+        #rp.voivodeship()
+        rp.fill_street(valid_street)
+        rp.fill_building_number(valid_building_number)
+        rp.fill_postal_code(valid_postal_code)
+        rp.fill_city(valid_city)
+        rp.fill_phone_number(valid_phone_number)
+        rp.fill_email(valid_email)
+        rp.fill_password(valid_password)
+        rp.fill_password_confirmation(valid_password_confirmation)
+        rp.accept_agreement()
+        rp.accept_rodo()
+        rp.create_account()
+        rp.verify_visible_errors(1, ["Podaj imię"])
+
+    def test_empty_surname(self):
+        rp = RegistrationPage(self.driver)
+        rp.fill_name(valid_name)
+        rp.choose_nationality()
+        #rp.voivodeship()
+        rp.fill_street(valid_street)
+        rp.fill_building_number(valid_building_number)
+        rp.fill_postal_code(valid_postal_code)
+        rp.fill_city(valid_city)
+        rp.fill_phone_number(valid_phone_number)
+        rp.fill_email(valid_email)
+        rp.fill_password(valid_password)
+        rp.fill_password_confirmation(valid_password_confirmation)
+        rp.accept_agreement()
+        rp.accept_rodo()
+        rp.create_account()
+        rp.verify_visible_errors(1, ["Podaj nazwisko"])
+
+    def test_space_like_a_street(self):
+        rp = RegistrationPage(self.driver)
+        rp.fill_name(valid_name)
+        rp.fill_surname(valid_surname)
+        rp.choose_nationality()
+        #rp.voivodeship()
+        rp.fill_street(" ")
+        rp.fill_building_number(valid_building_number)
+        rp.fill_postal_code(valid_postal_code)
+        rp.fill_city(valid_city)
+        rp.fill_phone_number(valid_phone_number)
+        rp.fill_email(valid_email)
+        rp.fill_password(valid_password)
+        rp.fill_password_confirmation(valid_password_confirmation)
+        rp.accept_agreement()
+        rp.accept_rodo()
+        rp.create_account()
+        rp.verify_visible_errors(1, ["Podaj ulicę"])
+
+    def test_incorrect_password(self):
+        rp = RegistrationPage(self.driver)
+        rp.fill_name(valid_name)
+        rp.fill_surname(valid_surname)
+        rp.choose_nationality()
+        #rp.voivodeship()
+        rp.fill_street(valid_street)
+        rp.fill_building_number(valid_building_number)
+        rp.fill_postal_code(valid_postal_code)
+        rp.fill_city(valid_city)
+        rp.fill_phone_number(valid_phone_number)
+        rp.fill_email(valid_email)
+        rp.fill_password(invalid_password)
+        rp.fill_password_confirmation(invalid_password_confirmation)
+        rp.accept_agreement()
+        rp.accept_rodo()
+        rp.create_account()
+        rp.verify_visible_errors(1, ["Podane hasło jest zbyt krótkie! Hasło musi zawierać przynajmniej 8 znaków"])
+
+
+
+sleep(20)
+
+if __name__=="__main__":
+    unittest.main(verbosity=2)
